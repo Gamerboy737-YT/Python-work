d3afad20e374e2b1c3583bffd91503e582e6005c
Gamerboy737-YT <117544600+Gamerboy737-YT@users.noreply.github.com>
2023-02-21 14:50:18 +0000

    Update Coursework.py

--- Coursework.py
+++ Coursework.py
@@ -10,5 +10,23 @@ flavours = {1: "Caramel Twist",
             10: "Coconut Dream"
             }
 
-def view_flavours:
-  print("Here are the flavours we have available:" \n{flavours})
+def view_flavours():
+            print("Here are the flavours we have available: \n{flavours.values()}")
+
+def select_tin_flavours():
+            user_choice = int(input("If you would like to view our available flavours, press 1. If you are ready to choose the flavours you want, press 2."):
+                if user_choice = 1:
+                      view_flavours()
+                      return select_tin_flavours()
+                elif user_choice = 2:
+                        return select_tin_flavours()
+
+            valid = False
+            while valid == False:
+                              try:
+                              flavour_to_add = input("Please either enter the corresponding number or name of the flavour to add: ")
+                              if flavour_to_add.title() not in flavours.values():
+                              print("Sorry, we don't recognsie that flavour. Please try again.")
+                              raise ValueError
+                        
+           
