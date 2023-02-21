54a92e4cdedc6fdd490d2cf93eb13caff6f14258
Gamerboy737-YT <117544600+Gamerboy737-YT@users.noreply.github.com>
2023-02-21 09:36:03 +0000

    Create Coursework.py

---
+++ Coursework.py
@@ -0,0 +1,14 @@
+flavours = {1: "Caramel Twist",
+            2: "Orange Crush",
+            3: "Chocolate Bar",
+            4: "Brazil Nut in Chocolate",
+            5: "Cornish Fudge", 
+            6: "Strawberry Treat",
+            7: "Orange Smoothie", 
+            8: "Toffee Bar", 
+            9: "Hazelnut Triangle", 
+            10: "Coconut Dream"
+            }
+
+def view_flavours:
+  print("Here are the flavours we have available:" \n{flavours})
